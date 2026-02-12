5c6084cfb9e91168e48cb4a4d9cfafd5edc3ae97
nieok1234-dev <nieok1234@gmail.com>
2026-02-12 16:20:30 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,54 @@
 let heartInterval;
+let starInterval;
 
-// SERDUSZKA
+// SERDUSZKO
 function createHeart() {
     const heart = document.createElement("div");
     heart.classList.add("heart");
     heart.innerHTML = "❤️";
-
     heart.style.left = Math.random() * 100 + "vw";
     heart.style.animationDuration = (3 + Math.random() * 3) + "s";
-
     document.body.appendChild(heart);
-
     setTimeout(() => heart.remove(), 6000);
 }
 
-// 3x WIĘCEJ SERDUSZEK
-function startManyHearts() {
-    clearInterval(heartInterval);
-    heartInterval = setInterval(createHeart, 100);
+// GWIAZDA
+function createStar() {
+    const star = document.createElement("div");
+    star.classList.add("star");
+    star.innerHTML = "✨";
+    star.style.left = Math.random() * 100 + "vw";
+    star.style.animationDuration = (3 + Math.random() * 3) + "s";
+    document.body.appendChild(star);
+    setTimeout(() => star.remove(), 6000);
 }
 
-// NORMALNA ILOŚĆ
+// 5x WIĘCEJ SERDUSZEK
+function startMegaHearts() {
+    clearInterval(heartInterval);
+    heartInterval = setInterval(createHeart, 60);
+}
+
+// NORMAL
 function startNormalHearts() {
     clearInterval(heartInterval);
     heartInterval = setInterval(createHeart, 300);
 }
 
-// PRZEJŚCIE MIĘDZY EKRANAMI
+// GWIAZDY
+function startStars() {
+    clearInterval(starInterval);
+    starInterval = setInterval(createStar, 300);
+}
+
+// PRZEJŚCIE
 function nextScreen() {
     document.getElementById("screen1").classList.remove("active");
     document.getElementById("screen2").classList.add("active");
+
     startNormalHearts();
+    startStars();
 }
 
 // START
-startManyHearts();
+startMegaHearts();
